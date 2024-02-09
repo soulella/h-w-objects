44660928159caa4c75f3cb9d3915ed2db51778b2
soulella <152424081+soulella@users.noreply.github.com>
2024-02-09 20:29:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# Task_1
+name = input("Enter name: ")
+person = {
+    "Darth Vader": "father",
+    "Leia": "sister",
+    "Han": "brother in law",
+    "R2D2": "droid"
+}
+def find_relatives(ism, inson):
+    for people in inson.keys():
+        if ism == people:
+            print(f"Luke i'm your {inson[people]}")
+            break
+    else:
+        print("Nope")
+find_relatives(name, person)
+
+
 #Task_2
 # https://edabit.com/challenge/ff2R7SymbB6PfjpPm
 budjet =[
